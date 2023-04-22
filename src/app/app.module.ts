884f8ee52ee7b44e638ef7696474beb0128c4951
pjroggy <pierrejeanroggy@gmail.com>
2023-04-22 17:39:31 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { ContactComponent } from "./contact/contact.component";
+
+const ROUTES: Routes = [
+    { path: 'home', 
+      component: HomeComponent      
+      },
+    { path: 'contact',      
+        component: ContactComponent       
+      },
+];
+
+export { ROUTES};
