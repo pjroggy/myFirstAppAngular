89b4296532142c3757d2b7ac662232f66a10ce4e
pjroggy <pierrejeanroggy@gmail.com>
2023-04-26 11:51:23 +0200

    challengeinput

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeveloperComponent } from './developer/developer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:DeveloperComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
